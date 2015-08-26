9a4779bb5d39b2b9d0f76c2091cdf3df1337d9d3
leonard-lin <linle.job@gmail.com>
2015-08-26 16:37:48 -0700

    process implemention
    after user uploaded pic,
    copy file to albums,
    generate thumbnail,
    redirect url to "/browse" which is a page browses all the photos.

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var multer = require('multer');
 var directory = require('serve-index');
 var cf = require('./htdoc/script/copyFile.js');
-
+var gt = require('./htdoc/script/getThumb.js');
+var browse = require('./htdoc/script/browse.js');
 var app = express();
 
 
@@ -13,17 +14,30 @@ app.use(multer({
 	dest: './uploads'
 }).single('image-file'));
 
+
+app.get('/', function(request, response) {
+		response.redirect('/upload.html');
+});
+
+app.get('/browse', function(request, response) {
+		browse.browse(request, response);
+});
+
 app.post('/upload', function(request, response) {
 	var src = request.file.path;
-	var dst = "./htdoc/albums/album1/"+ request.file.filename + ".jpg";
+	var dst = "./htdoc/albums/album1/" + request.file.filename + ".jpg";
 	console.log(src);
 	cf.copyFile(src, dst, function() {
-    	console.log("copying is done!");
-    	response.redirect("/upload.html");
+		var thumbPath = "./htdoc/albums/album1/thumb-" + request.file.filename + ".jpg";
+		console.log("copying is done.");
+		gt.genThumb(src, thumbPath, function(err){
+			if(err) throw err;
+			response.redirect('/browse');
+		})
 	});
 });
+
 
 app.listen(8080, function() {
 	console.log('listening on port 8080 ...');
 });
-
